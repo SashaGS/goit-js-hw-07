66cf1137b905ad5c5ae1c77f8727b3496bcaff32
SashaGS <Fqva@ukr.net>
2026-05-18 22:24:52 +0300

    fix task-1 all rigth

--- js/task-1.js
+++ js/task-1.js
@@ -1,19 +1,41 @@
-// Знаходимо ul#categories
+// // Знаходимо ul#categories
+// const categoriesList = document.querySelector('#categories');
+
+// // Знаходимо всі li.item всередині нього
+// const categoriesItems = categoriesList.querySelectorAll('li.category');
+
+// // console.log(typeof(categoriesItems));
+
+// // Виводимо кількість категорій
+// console.log(`Number of categories: ${categoriesItems.length}`);
+
+// // Для кожного li.item виводимо заголовок і кількість елементів
+// categoriesItems.forEach(item => {
+//   const title = item.querySelector('h2').textContent;
+//   const elementsCount = item.querySelectorAll('li').length;
+  
+//   console.log(`Category: ${title}`);
+//   console.log(`Elements: ${elementsCount}`);
+// });
+
+// Знаходимо список категорій
 const categoriesList = document.querySelector('#categories');
 
-// Знаходимо всі li.item всередині нього
-const categoriesItems = categoriesList.querySelectorAll('li.category');
-
-// console.log(typeof(categoriesItems));
+// Знаходимо всі елементи li.item
+const categoryItems = categoriesList.querySelectorAll('li.item');
 
 // Виводимо кількість категорій
-console.log(`Number of categories: ${categoriesItems.length}`);
+console.log(`Number of categories: ${categoryItems.length}`);
 
-// Для кожного li.item виводимо заголовок і кількість елементів
-categoriesItems.forEach(item => {
+// Для кожного елемента li.item
+categoryItems.forEach(item => {
+  // Знаходимо заголовок <h2>
   const title = item.querySelector('h2').textContent;
-  const elementsCount = item.querySelectorAll('li').length;
-  
+
+  // Знаходимо всі вкладені <li> всередині цього елемента
+  const elementsCount = item.querySelectorAll('ul li').length;
+
+  // Виводимо результат
   console.log(`Category: ${title}`);
   console.log(`Elements: ${elementsCount}`);
 });
