158df7e1f44e754c34005ceefdf5e52152becf69
SashaGS <Fqva@ukr.net>
2026-05-18 14:22:03 +0300

    done task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault(); 
+    
+    const emailValue    = form.elements['email'].value; 
+    const passwordValue = form.elements['password'].value;
+
+    if (emailValue === '' || passwordValue === '') {
+        alert("All form fields must be filled in");
+    }
+
+    const dataForm = new FormData(form);
+    const data ={
+        email:   dataForm.get('email').trim(),
+        password:dataForm.get('password').trim(),
+    };
+
+    console.log(data);
+    form.reset();
+    
+});
+
